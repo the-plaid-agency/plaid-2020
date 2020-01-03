6e3cfe82ba98e1c0d1b8679560cd2593fb70e14f
xenostar <bryan.a.leewood@gmail.com>
2020-01-03 13:39:34 -0500

    Adding more plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,9 @@ require('dotenv').config()
 module.exports = {
   siteMetadata: {
     title: `THE PLAID AGENCY 2020`,
+    description: `The official website of THE PLAID AGENCY.`,
+    author: `THE PLAID AGENCY`,
+    siteUrl: `https://plaid-2020.netlify.com/`,
   },
   plugins: [
     {
@@ -16,5 +19,29 @@ module.exports = {
         apiToken: process.env.DATO_API_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `THE PLAID AGENCY`,
+        short_name: `THE PLAID AGENCY`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/assets/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-alias-imports`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`
   ]
 }
