bd4873b19b38168fbf23878bb3bfabf691473fda
xenostar <bryan.a.leewood@gmail.com>
2020-01-10 11:03:34 -0500

    Omg just work

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 import { Image } from 'components'
 import AniLink_ from 'gatsby-plugin-transition-link/AniLink'
 // import logoPlaid from 'assets/logo_plaid_1.svg'
+import logoPlaid from 'assets/logo_plaid_1.png'
 
 export const Logo = () => (
   <AniLink cover direction="left" bg="#FF671D" to="/">
-    {/* <Image src={logoPlaid} alt="nba-ref" /> */}
-    <Image src="/logo_plaid_1.svg" alt="nba-ref" />
+    <Image src={logoPlaid} alt="nba-ref" />
+    {/* <Image src="/logo_plaid_1.svg" alt="nba-ref" /> */}
   </AniLink>
 )
 
