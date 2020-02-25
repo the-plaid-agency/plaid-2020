504838165ac76e10556c066183c594ab7238cc5b
xenostar <bryan.a.leewood@gmail.com>
2020-02-24 20:08:18 -0500

    Starting some work on logo/nav color changing

--- src/components/TopBar/TopBarBurger.js
+++ src/components/TopBar/TopBarBurger.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { useStoreActions } from 'easy-peasy'
 import { GiHamburgerMenu as GiHamburgerMenu_ } from 'react-icons/gi'
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
 
 export const TopBarBurger = () => {
   const openNav = useStoreActions(actions => actions.nav.openNav)
+  const [hideOnScroll, setHideOnScroll] = useState(true)
 
-  return <GiHamburgerMenu onClick={openNav} />
+  useScrollPosition(({ prevPos, currPos }) => {
+    const isShow = currPos.y > prevPos.y
+    if (isShow !== hideOnScroll) setHideOnScroll(isShow)
+  }, [hideOnScroll], false, false, 300)
+
+  return <GiHamburgerMenu show={hideOnScroll ? 1 : 0} onClick={openNav} />
 }
 
 const GiHamburgerMenu = styled(GiHamburgerMenu_)`
-  color: ${props => props.theme.colors.white};
+  color: ${props => props.show ? props.theme.colors.white : props.theme.colors.primary};
   cursor: pointer;
   font-size: 1.625em; /* 26px */
   pointer-events: auto;
